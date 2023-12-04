fd34b4e14953f584239485db1e5b8760c8b44c5f
Franck <Franck.Stone@protonmail.com>
2023-12-04 09:55:07 +0800

    add wodge withdraw

--- explorer/wdoge.go
+++ explorer/wdoge.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/dogecoinw/doged/btcjson"
+	"github.com/dogecoinw/doged/chaincfg/chainhash"
 	"github.com/google/uuid"
 	"github.com/unielon-org/unielon-indexer/utils"
 )
@@ -21,40 +22,63 @@ func (e Explorer) wdogeDecode(tx *btcjson.TxRawResult, pushedData []byte, number
 		return nil, fmt.Errorf("ConvetSwap err: %s", err.Error())
 	}
 
-	if len(tx.Vout) != 3 {
+	if len(tx.Vout) > 0 {
 		return nil, fmt.Errorf("mint op error, vout length is not 2")
 	}
 
-	fee := int64(0)
-	if wdoge.Amt.Int64()*3/1000 < 50000000 {
-		fee = 50000000
-	} else {
-		fee = wdoge.Amt.Int64() * 3 / 1000
-	}
+	if wdoge.Op == "deposit" {
+		if len(tx.Vout) != 3 {
+			return nil, fmt.Errorf("mint op error, vout length is not 2")
+		}
 
-	if int64(tx.Vout[1].Value*100000000) != wdoge.Amt.Int64() {
-		return nil, fmt.Errorf("The fee is not enough")
-	}
+		fee := int64(0)
+		if wdoge.Amt.Int64()*3/1000 < 50000000 {
+			fee = 50000000
+		} else {
+			fee = wdoge.Amt.Int64() * 3 / 1000
+		}
 
-	if tx.Vout[1].ScriptPubKey.Addresses[0] != wdogeCoolAddress {
-		return nil, fmt.Errorf("The address is incorrect")
-	}
+		if int64(tx.Vout[1].Value*100000000) != wdoge.Amt.Int64() {
+			return nil, fmt.Errorf("The fee is not enough")
+		}
 
-	if int64(tx.Vout[2].Value*100000000) != fee {
-		return nil, fmt.Errorf("The fee is not enough")
-	}
+		if tx.Vout[1].ScriptPubKey.Addresses[0] != wdogeCoolAddress {
+			return nil, fmt.Errorf("The address is incorrect")
+		}
 
-	if tx.Vout[2].ScriptPubKey.Addresses[0] != wdogeFeeAddress {
-		return nil, fmt.Errorf("The address is incorrect")
+		if int64(tx.Vout[2].Value*100000000) != fee {
+			return nil, fmt.Errorf("The fee is not enough")
+		}
+
+		if tx.Vout[2].ScriptPubKey.Addresses[0] != wdogeFeeAddress {
+			return nil, fmt.Errorf("The address is incorrect")
+		}
+	} else if wdoge.Op == "withdraw" {
+
 	}
 
 	wdoge.OrderId = uuid.New().String()
 	wdoge.FeeTxHash = tx.Vin[0].Txid
-
 	wdoge.WDogeTxHash = tx.Hash
 	wdoge.WDogeBlockHash = tx.BlockHash
 	wdoge.WDogeBlockNumber = number
 	wdoge.HolderAddress = tx.Vout[0].ScriptPubKey.Addresses[0]
+
+	txhash0, _ := chainhash.NewHashFromStr(tx.Vin[0].Txid)
+	txRawResult0, err := e.node.GetRawTransactionVerboseBool(txhash0)
+	if err != nil {
+		return nil, chainNetworkErr
+	}
+
+	txhash1, _ := chainhash.NewHashFromStr(txRawResult0.Vin[0].Txid)
+	txRawResult1, err := e.node.GetRawTransactionVerboseBool(txhash1)
+	if err != nil {
+		return nil, chainNetworkErr
+	}
+
+	if wdoge.HolderAddress != txRawResult1.Vout[txRawResult0.Vin[0].Vout].ScriptPubKey.Addresses[0] {
+		return nil, fmt.Errorf("The address is not the same as the previous transaction")
+	}
 
 	wdoge1, err := e.dbc.FindWDogeInfoByTxHash(wdoge.WDogeTxHash)
 	if err != nil {
