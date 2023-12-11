53eb68ac56ba28baea253789c945113b57503984
Franck <Franck.Stone@protonmail.com>
2023-12-11 23:44:18 +0700

    update test

--- mint_test.go
+++ mint_test.go
@@ -200,7 +200,7 @@ func TestMintDeploy(t *testing.T) {
 	builder.AddOp(txscript.OP_1).AddData(wif.PrivKey.PubKey().SerializeCompressed()).AddOp(txscript.OP_1)
 	builder.AddOp(txscript.OP_CHECKMULTISIGVERIFY)
 	builder.AddData([]byte("ord")).AddData([]byte("text/plain;charset=utf-8")).AddData(jsonData)
-	builder.AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP)
+	builder.AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP)
 
 	// redeem script is the script program in the format of []byte
 	redeemScript, err := builder.Script()
@@ -329,7 +329,7 @@ func TestSwap(t *testing.T) {
 	builder.AddOp(txscript.OP_1).AddData(wif.PrivKey.PubKey().SerializeCompressed()).AddOp(txscript.OP_1)
 	builder.AddOp(txscript.OP_CHECKMULTISIGVERIFY)
 	builder.AddData([]byte("ord")).AddData([]byte("text/plain;charset=utf-8")).AddData(jsonData)
-	builder.AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP)
+	builder.AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP)
 
 	// redeem script is the script program in the format of []byte
 	redeemScript, err := builder.Script()
@@ -422,7 +422,7 @@ func TestWdoge(t *testing.T) {
 	builder.AddOp(txscript.OP_1).AddData(wif.PrivKey.PubKey().SerializeCompressed()).AddOp(txscript.OP_1)
 	builder.AddOp(txscript.OP_CHECKMULTISIGVERIFY)
 	builder.AddData([]byte("ord")).AddData([]byte("text/plain;charset=utf-8")).AddData(jsonData)
-	builder.AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP)
+	builder.AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP).AddOp(txscript.OP_DROP)
 
 	// redeem script is the script program in the format of []byte
 	redeemScript, err := builder.Script()
